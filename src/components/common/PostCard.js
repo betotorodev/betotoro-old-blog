655d066d33ae1c060baa82504b294664412b5fb3
Beto <betotoro0902@gmail.com>
2020-08-28 18:11:27 -0500

    [FIX] code colors

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import { Tags } from '@tryghost/helpers-gatsby'
+// import { Tags } from '@tryghost/helpers-gatsby'
 // import { readingTime as readingTimeHelper } from '@tryghost/helpers'
 
 const PostCard = ({ post }) => {
